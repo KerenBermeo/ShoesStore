f5b6c396491470a3d99a582adbb83c1054c1cb25
Ljohanna990 <johannats990@gmail.com>
2024-09-20 21:25:15 -0500

    Entidades permisos y roles

--- src/main/java/com/shoesstore/app/models/PermissionEntity.java
+++ src/main/java/com/shoesstore/app/models/PermissionEntity.java
@@ -1,5 +1,26 @@
 package com.shoesstore.app.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "permissions")
+
 public class PermissionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "permissions")
+    private Set<RolEntity> roles;
  
 }
--- src/main/java/com/shoesstore/app/models/RolEntity.java
+++ src/main/java/com/shoesstore/app/models/RolEntity.java
@@ -1,14 +1,35 @@
 package com.shoesstore.app.models;
 
-public class RolEntity {
-    private int id;
-    private Rol name;
-    private Set<PermissionEntity> permission;
 
-    public RolEntity(Rol name){
-        this.name = name;
-    }
-    public Rol getRol(){
-        return name;
-    }
+import jakarta.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+
+public class RolEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String nombre;
+
+    @ManyToMany
+    @JoinTable(
+        name = "rol_permissions",
+        joinColumns = @JoinColumn(name = "rol_id"),
+        inverseJoinColumns = @JoinColumn(name = "permissions_id")
+    )
+    private Set<PermissionEntity> permission;
 }
+
+
